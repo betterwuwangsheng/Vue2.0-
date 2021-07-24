ca0d77d98fef2563810221e82784310bc97ea1b4
betterwuwangsheng <349807102@qq.com>
2021-07-24 23:58:20 +0800

    配置全局样式、项目基本工作准备

---
+++ account-module-02/src/project/food/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id='app'>
+    food 项目
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang='scss'>
+  #app{
+    color:#ff0000;
+  }
+</style>
---
+++ account-module-02/src/project/pc/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    pc 项目
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
